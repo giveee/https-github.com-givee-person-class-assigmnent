e9b3f15cc1eab17a2e73dc2c3e2fe1084f6e08a9
Giveee <mphala26@gmail.com>
2024-08-23 00:27:28 +0200

    Add files via upload

---
+++ person.py
@@ -0,0 +1,11 @@
+class person:
+    def_init_(self,name,age,gender):
+    self.name=Name
+    self.age=age
+    self.gender=gender
+
+    def introduce(self);
+        print(m"hello, name is {Given} and i am a {man}.")
+
+        person1=person(name="Given", age="28",gender="male")
+        person1.introduce()
